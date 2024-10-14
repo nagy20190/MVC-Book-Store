8446ac9fb72088a292c02240084bba5c68783809
nagy20190 <mostafanagy679@gmail.com>
2024-10-14 03:18:56 +0300

    UpdateThings

--- Book_Store_Project/BStore/BKStore-MVC/obj/Debug/net8.0/BKStore-MVC.AssemblyInfo.cs
+++ Book_Store_Project/BStore/BKStore-MVC/obj/Debug/net8.0/BKStore-MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BKStore-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ac17b66a788cd3f86fc24ed029983f479454f15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4184b0eb0279b3056c7d7f543f711854329963a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BKStore-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BKStore-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
